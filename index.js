c6b8c15efaa802ff7192e48ba19366961e727383
ajayduth <ajayduth123@gmail.com>
2020-12-22 20:33:16 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const studentRouter = require("../src/routers/studentRouter");
-const connectDB = require("../src/db/db");
+const studentRouter = require("./src/routers/studentRouter");
+const connectDB = require("./src/db/db");
 const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
