c43c9d8f7cd8c6b769d5bedf09f3e699b187518a
ajay <ajayduth123@gmail.com>
2021-04-22 22:35:12 +0530

    fixed multiple registraion

--- src/routers/studentRouter.js
+++ src/routers/studentRouter.js
@@ -5,7 +5,7 @@ const Student = require("../models/student");
 const controller = require("../controllers/studentControllers");
 const { verifyAccessToken } = require("../controllers/authContoller");
 
-router.get("/student", async (req, res) => {
+router.get("/student",verifyAccessToken, async (req, res) => {
   controller.readAll(req, res);
 });
 
